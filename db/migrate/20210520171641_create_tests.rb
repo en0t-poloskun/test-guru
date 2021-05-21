b00f5748f36ecae9562600a46fb3685afdedb8c3
Мария <golovneva.maria@yandex.ru>
2021-05-21 12:57:16 +0300

    references for tables added

--- db/migrate/20210520171641_create_tests.rb
+++ db/migrate/20210520171641_create_tests.rb
@@ -5,6 +5,8 @@ class CreateTests < ActiveRecord::Migration[6.1]
     create_table :tests do |t|
       t.string :name, null: false
       t.integer :level, default: 0
+      t.references :category, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -19,8 +19,10 @@ ActiveRecord::Schema.define(version: 20_210_520_173_139) do
   create_table 'answers', force: :cascade do |t|
     t.string 'body', null: false
     t.boolean 'correct', default: false
+    t.bigint 'question_id', null: false
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
+    t.index ['question_id'], name: 'index_answers_on_question_id'
   end
 
   create_table 'categories', force: :cascade do |t|
@@ -31,15 +33,21 @@ ActiveRecord::Schema.define(version: 20_210_520_173_139) do
 
   create_table 'questions', force: :cascade do |t|
     t.string 'body', null: false
+    t.bigint 'test_id', null: false
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
+    t.index ['test_id'], name: 'index_questions_on_test_id'
   end
 
   create_table 'tests', force: :cascade do |t|
     t.string 'name', null: false
     t.integer 'level', default: 0
+    t.bigint 'category_id', null: false
+    t.bigint 'user_id', null: false
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
+    t.index ['category_id'], name: 'index_tests_on_category_id'
+    t.index ['user_id'], name: 'index_tests_on_user_id'
   end
 
   create_table 'users', force: :cascade do |t|
@@ -51,4 +59,9 @@ ActiveRecord::Schema.define(version: 20_210_520_173_139) do
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
   end
+
+  add_foreign_key 'answers', 'questions'
+  add_foreign_key 'questions', 'tests'
+  add_foreign_key 'tests', 'categories'
+  add_foreign_key 'tests', 'users'
 end
