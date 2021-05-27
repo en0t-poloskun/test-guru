d5ed5390db7e468d714a26cde0d5b1d736164e50
Мария <golovneva.maria@yandex.ru>
2021-05-27 22:36:42 +0300

    find_tests_names method added

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 class Test < ApplicationRecord
+  def self.find_tests_names(category)
+    category = Category.find_by(name: category)
+    Test.where(category_id: category.id).order(name: :desc).pluck(:name)
+  end
 end
