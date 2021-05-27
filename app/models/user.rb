d0bf2b133d141d07f7b3ab0c8a3665406ec669c8
Мария <golovneva.maria@yandex.ru>
2021-05-27 22:11:50 +0300

    find_tests method added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  def find_tests(level)
+    started_tests = Result.where(user_id: id).pluck(:test_id)
+    Test.where(level: level, id: started_tests)
+  end
 end
