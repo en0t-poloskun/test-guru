62f8e74516f5ddd6c1e40a3e9c38dbef83bd0f19
Мария <golovneva.maria@yandex.ru>
2021-06-02 13:43:47 +0300

    find_tests method fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@
 
 class User < ApplicationRecord
   def find_tests(level)
-    Test.joins('JOIN results ON tests.id = results.test_id').where('results.user_id': id, level: level)
+    Test
+      .joins('JOIN results ON tests.id = results.test_id')
+      .where(results: { user_id: id }, level: level)
   end
 end
