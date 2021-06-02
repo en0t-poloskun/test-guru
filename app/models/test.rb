ef3e85537fa54071f48f6e4c7e06113b2a7f6836
Мария <golovneva.maria@yandex.ru>
2021-06-02 13:40:41 +0300

    find_tests_names method fixed

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,7 +2,9 @@
 
 class Test < ApplicationRecord
   def self.find_tests_names(category)
-    Test.joins('JOIN categories ON tests.category_id = categories.id').where('categories.name': category)
-        .order(name: :desc).pluck(:name)
+    joins('JOIN categories ON tests.category_id = categories.id')
+      .where(categories: { name: category })
+      .order(name: :desc)
+      .pluck(:name)
   end
 end
