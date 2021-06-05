87bc16779a0f74faf17afe38e70629c7d6044bf2
Мария <golovneva.maria@yandex.ru>
2021-06-05 13:39:09 +0300

    rails style fixes

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,10 +2,11 @@
 
 class Test < ApplicationRecord
   belongs_to :category
+  belongs_to :author, class_name: 'User'
+
   has_many :questions
   has_many :results
   has_many :users, through: :results
-  belongs_to :author, class_name: 'User'
 
   def self.find_tests_names(category)
     joins('JOIN categories ON tests.category_id = categories.id')
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_many :results
   has_many :tests, through: :results
   has_many :created_tests, foreign_key: 'author_id', class_name: 'Test'
+
   def find_tests(level)
     Test
       .joins('JOIN results ON tests.id = results.test_id')
