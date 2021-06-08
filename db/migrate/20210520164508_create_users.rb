1426a9d330e602424109c1556c28596c5b4a8ec1
Мария <golovneva.maria@yandex.ru>
2021-06-08 15:39:15 +0300

    email attribute added for user

--- db/migrate/20210520164508_create_users.rb
+++ db/migrate/20210520164508_create_users.rb
@@ -7,6 +7,7 @@ class CreateUsers < ActiveRecord::Migration[6.1]
       t.string :last_name, null: false
       t.string :login, null: false
       t.string :password, null: false
+      t.string :email, null: false
       t.string :role, null: false
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 20_210_527_181_901) do
     t.string 'last_name', null: false
     t.string 'login', null: false
     t.string 'password', null: false
+    t.string 'email', null: false
     t.string 'role', null: false
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
