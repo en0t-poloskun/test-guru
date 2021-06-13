96303d604ece4002edb59d20768b825e925b83a0
Мария <golovneva.maria@yandex.ru>
2021-06-13 20:33:23 +0300

    method for getting specific question added

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
 class QuestionsController < ApplicationController
-  before_action :find_test
+  before_action :find_test, only: %i[index]
 
   def index
     render plain: @test.questions.pluck(:body)
+  end
+
+  def show
+    render plain: Question.find(params[:id]).body
   end
 
   private
