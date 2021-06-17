cc2f9698e9ba191931b52a6da3d8db52a6f71a52
Мария <golovneva.maria@yandex.ru>
2021-06-17 22:13:03 +0300

    question_header moved from tests_helper to questions_helper

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 module QuestionsHelper
+  def question_header(test)
+    case params[:action]
+    when 'new'
+      "Create New #{test.name} Question"
+    when 'edit'
+      "Edit #{test.name} Question"
+    end
+  end
 end
