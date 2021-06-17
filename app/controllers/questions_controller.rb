c12afa758e0665db92a4b44eacb166ca23bc02f6
Мария <golovneva.maria@yandex.ru>
2021-06-17 18:57:04 +0300

    view for question editing created

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,7 +2,7 @@
 
 class QuestionsController < ApplicationController
   before_action :find_test, only: %i[index create new]
-  before_action :find_question, only: %i[show destroy]
+  before_action :find_question, only: %i[show destroy edit update]
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_record_not_found
 
   def index
@@ -15,12 +15,23 @@ class QuestionsController < ApplicationController
     @question = Question.new
   end
 
+  def edit
+  end
+
   def create
     @question = @test.questions.build(question_params)
     if @question.save
       redirect_to @question
     else
       render :new
+    end
+  end
+
+  def update
+    if @question.update(question_params)
+      redirect_to @question
+    else
+      render :edit
     end
   end
 
