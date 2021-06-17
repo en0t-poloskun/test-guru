6c2b73cd8d27faf23279b56efa6e2f034c674fed
Мария <golovneva.maria@yandex.ru>
2021-06-17 22:00:40 +0300

    header helper for new and edit views created

--- app/helpers/tests_helper.rb
+++ app/helpers/tests_helper.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 module TestsHelper
+  def question_header(test)
+    case params[:action]
+    when 'new'
+      "Create New #{test.name} Question"
+    when 'edit'
+      "Edit #{test.name} Question"
+    end
+  end
 end
