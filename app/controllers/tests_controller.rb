7f84230bc69373d3953728e800de6bb49a5140cc
Мария <golovneva.maria@yandex.ru>
2021-06-18 18:41:18 +0300

    methods and views for test updating created

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,17 +1,19 @@
 # frozen_string_literal: true
 
 class TestsController < ApplicationController
+  before_action :find_test, only: %i[show edit update]
+
   def index
     @test = Test.all
   end
 
-  def show
-    @test = Test.find(params[:id])
-  end
+  def show; end
 
   def new
     @test = Test.new
   end
+
+  def edit; end
 
   def create
     @test = Test.new(test_params)
@@ -22,7 +24,19 @@ class TestsController < ApplicationController
     end
   end
 
+  def update
+    if @test.update(test_params)
+      redirect_to @test
+    else
+      render :edit
+    end
+  end
+
   private
+
+  def find_test
+    @test = Test.find(params[:id])
+  end
 
   def test_params
     params.require(:test).permit(:name, :level, :category_id)
