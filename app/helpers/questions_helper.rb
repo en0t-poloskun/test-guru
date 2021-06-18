d2f091d84449937677ad4e7e9d68c7bf87eb42e8
Мария <golovneva.maria@yandex.ru>
2021-06-18 19:25:33 +0300

    params[:action] replaced by question.new_record? in question_header helper

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 
 module QuestionsHelper
-  def question_header(test)
-    case params[:action]
-    when 'new'
+  def question_header(test, question)
+    if question.new_record?
       "Create New #{test.name} Question"
-    when 'edit'
+    else
       "Edit #{test.name} Question"
     end
   end
