02bc8541a817164171c4d9d740f873033d333183
Мария <golovneva.maria@yandex.ru>
2021-06-21 16:17:34 +0300

    set_user callback removed from tests_controller

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -2,7 +2,6 @@
 
 class TestsController < ApplicationController
   before_action :find_test, only: %i[show edit update destroy start]
-  before_action :set_user, only: :start
 
   def index
     @test = Test.all
@@ -39,6 +38,7 @@ class TestsController < ApplicationController
   end
 
   def start
+    @user = User.first
     @user.tests.push(@test)
     redirect_to @user.test_passage(@test)
   end
@@ -47,10 +47,6 @@ class TestsController < ApplicationController
 
   def find_test
     @test = Test.find(params[:id])
-  end
-
-  def set_user
-    @user = User.first
   end
 
   def test_params
