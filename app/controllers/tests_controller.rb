8e7e95f8b045e80d6a24cee5a2c421434befbda4
Мария <golovneva.maria@yandex.ru>
2021-06-24 14:08:00 +0300

    controller and views for sessions created, methods for authentication added

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class TestsController < ApplicationController
+  before_action :authenticate_user!
   before_action :find_test, only: %i[show edit update destroy start]
 
   def index
