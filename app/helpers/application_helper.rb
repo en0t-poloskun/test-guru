66ae48091fd16e68002e0498c0dd2fda375c0bb2
Мария <golovneva.maria@yandex.ru>
2021-06-24 17:09:57 +0300

    flash_message helper added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,8 @@ module ApplicationHelper
   def github_url(author:, repo:, title:)
     link_to title, "https://github.com/#{author}/#{repo}", target: '_blank', rel: 'nofollow'
   end
+
+  def flash_message(message)
+    content_tag :p, flash[message], class: "flash #{message}" if flash[message]
+  end
 end
