a157254e3dc983de739dd6d39d49d081ed220d53
Мария <golovneva.maria@yandex.ru>
2021-06-28 00:27:53 +0300

    after_sign_in_path_for helper added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,11 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
+  private
+
+  def after_sign_in_path_for(user)
+    user.admin ? admin_tests_path : root_path
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:firstname, :lastname, :login, :email, :password) }
