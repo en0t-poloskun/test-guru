df143922513e1e54d113325715ae1f73397c6fda
Мария <golovneva.maria@yandex.ru>
2021-07-02 14:55:09 +0300

    question_header helper removed

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,11 +1,4 @@
 # frozen_string_literal: true
 
 module QuestionsHelper
-  def question_header(test, question)
-    if question.new_record?
-      t('.header', test: test.name)
-    else
-      t('.header', test: test.name)
-    end
-  end
 end
