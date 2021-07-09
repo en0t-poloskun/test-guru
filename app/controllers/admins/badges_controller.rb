0ed8aec44de78fc93cc9430b01a4b0a2b01c9a34
Мария <golovneva.maria@yandex.ru>
2021-07-09 16:35:53 +0300

    method show for Admins::BagesController implemented

--- app/controllers/admins/badges_controller.rb
+++ app/controllers/admins/badges_controller.rb
@@ -2,8 +2,18 @@
 
 module Admins
   class BadgesController < Admins::BaseController
+    before_action :set_badge, only: %i[show]
+
     def index
       @badges = Badge.all
+    end
+
+    def show; end
+
+    private
+
+    def set_badge
+      @badge = Badge.find(params[:id])
     end
   end
 end
